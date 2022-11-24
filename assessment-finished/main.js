d7c32f3fd74dad579a4d7281084c792ee2ef3f57
chanshiori15 <64536186+chanshiori15@users.noreply.github.com>
2022-11-24 19:13:25 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'その日は、とても暑かった。 so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const storyText = 'その日は、とても暑かった。 だから :insertx: 歩いた。そして :inserty:についた。 :insertz: 体重150キロの:insertx: は驚いた。天気予報は快晴だった。;
+const insertX = ['わたしは', 'おじいさんは', 'サンタクロースは'];
+const insertY = ['友達の家', 'ユニーバーサルスタジオ', '別荘'];
+const insertZ = ['とても暑かった。', '雪が降っていた。', '混んでいた。'];
 
 randomize.addEventListener('click', result);
 
