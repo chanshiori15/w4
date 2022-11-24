c00eeb4de8b4ef15c5b1172974eb2e1ebd888e32
chanshiori15 <64536186+chanshiori15@users.noreply.github.com>
2022-11-24 19:19:15 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'その日は、とても暑かった。 だから :insertx: 歩いた。そして :inserty:についた。 :insertz: 体重150キロの:insertx: は驚いた。天気予報は快晴だった。';
+const storyText = 'その日は、とても暑かった。 だから :insertx: 歩いた。そして :inserty: についた。 :insertz: 体重150キロの :insertx: は驚いた。天気予報は快晴だった。';
 const insertX = ['わたしは', 'おじいさんは', 'サンタクロースは'];
 const insertY = ['友達の家', 'ユニーバーサルスタジオ', '別荘'];
 const insertZ = ['とても暑かった。', '雪が降っていた。', '混んでいた。'];
